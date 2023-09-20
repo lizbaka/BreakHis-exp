6574b806d9f013f8bbc1ebde4e85d5afdce7de5b
lizbaka <1849214604@qq.com>
2023-09-20 11:48:12 +0800

    update networks and tasks examples

--- networks.py
+++ networks.py
@@ -17,3 +17,19 @@ class ResNet50(nn.Module):
     def forward(self, x):
         x = self.resnet(x)
         return x
+
+
+class ResNet50_Pretrain(nn.Module):
+    def __init__(self, num_classes):
+        super(ResNet50_Pretrain, self).__init__()
+        self.num_classes = num_classes
+        
+        self.resnet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        
+        # 替换最后一层全连接层
+        num_features = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_features, num_classes)
+        
+    def forward(self, x):
+        x = self.resnet(x)
+        return x
